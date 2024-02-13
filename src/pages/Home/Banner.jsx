cc98d7f49509a09a84f30118adddaa601d7fb117
Shahriar <shahriarh655@gmail.com>
2024-02-13 23:04:14 +0600

    Added banner section

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import bgImg1 from "../../../src/assets/Banner/banner-1.jpg";
+import bgImg2 from "../../../src/assets/Banner/banner-2.jpg";
+import bgImg3 from "../../../src/assets/Banner/banner-3.jpg";
+const Banner = () => {
+  return (
+    <>
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item-wrapper">
+          <div className="banner-img-overlay"></div>
+          <img src={bgImg1} className="w-full" />
+          {/* texts */}
+          <div className="banner-texts">
+            <div className="texts-container">
+              <p className="banner-sub-heading">Bringing Class To Cuisine</p>
+              <h1 className="banner-heading">
+                Where every flavor tells a story
+              </h1>
+              <p className="banner-description">
+                Craving some delicious pairs food? Maybe your are in the mood
+                for a juicy steak? No matter what kind of meal you have in
+                mind.The Brochette Restaurant is ready to prepare it for you
+              </p>
+              <div>
+                <button className="banner-btn">Discover Menu</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="item2" className="carousel-item-wrapper">
+          <div className="banner-img-overlay"></div>
+          <img src={bgImg2} className="w-full" />
+          {/* texts */}
+          <div className="banner-texts">
+            <div className="texts-container">
+              <p className="banner-sub-heading">Bringing Class To Cuisine</p>
+              <h1 className="banner-heading">
+                Where every flavor tells a story
+              </h1>
+              <p className="banner-description">
+                Craving some delicious pairs food? Maybe your are in the mood
+                for a juicy steak? No matter what kind of meal you have in
+                mind.The Brochette Restaurant is ready to prepare it for you
+              </p>
+              <div>
+                <button className="banner-btn">Discover Menu</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="item3" className="carousel-item-wrapper">
+          <div className="banner-img-overlay"></div>
+          <img src={bgImg3} className="w-full" />
+          {/* texts */}
+          <div className="banner-texts">
+            <div className="texts-container">
+              <p className="banner-sub-heading">Bringing Class To Cuisine</p>
+              <h1 className="banner-heading">
+                Where every flavor tells a story
+              </h1>
+              <p className="banner-description">
+                Craving some delicious pairs food? Maybe your are in the mood
+                for a juicy steak? No matter what kind of meal you have in
+                mind.The Brochette Restaurant is ready to prepare it for you
+              </p>
+              <div>
+                <button className="banner-btn">Discover Menu</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
