2d414f585277c10ac93f0111ca969f28534c2be3
Shahriar <shahriarh655@gmail.com>
2024-02-27 16:07:06 +0600

    Added our services section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner";
 import AboutRestaurant from "../../AboutRestaurant/AboutRestaurant";
+import OurServices from "../OurServices/OurServices";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <AboutRestaurant />
+      <OurServices />
     </div>
   );
 };
---
+++ src/pages/Home/OurServices/OurServices.jsx
@@ -0,0 +1,65 @@
+import bgImg from "../../../assets/Banner/bg-box_icon.jpg";
+
+import chefIcon from "../../../assets/about-us/chef.png";
+import foodIcon from "../../../assets/about-us/food.png";
+import musicIcon from "../../../assets/about-us/music.png";
+import winesIcon from "../../../assets/about-us/wines.png";
+
+const OurServices = () => {
+  return (
+    <section
+      className="section-wrapper"
+      style={{ backgroundImage: `url(${bgImg})` }}
+    >
+      <div className="our-services-container ">
+        {/* div-1 */}
+        <div className="food space-y-5 text-center">
+          <div className="icon-wrapper">
+            <img src={foodIcon} alt="" />
+          </div>
+          <h2 className="service-heading">Food Is Always Fresh</h2>
+          <p className="service-des">
+            The food we choose is always fresh and carefully checked before
+            processing
+          </p>
+        </div>
+        {/* div-2 */}
+        <div className="chef space-y-5 text-center">
+          <div className="icon-wrapper">
+            <img src={chefIcon} alt="" />
+          </div>
+          <h2 className="service-heading">Long Time Chef</h2>
+          <p className="service-des">
+            The best Chefs in the world always have their own secrets for their
+            dishes
+          </p>
+        </div>
+        {/* div-3 */}
+        <div className="music space-y-5 text-center">
+          {" "}
+          <div className="icon-wrapper">
+            <img src={musicIcon} alt="" />
+          </div>
+          <h2 className="service-heading">Luxury Space & Music</h2>
+          <p className="service-des">
+            What's better than enjoying delicious food and music at the same
+            time
+          </p>
+        </div>
+        {/* div-4 */}
+        <div className="wines space-y-5 text-center">
+          <div className="icon-wrapper">
+            <img src={winesIcon} alt="" />
+          </div>
+          <h2 className="service-heading">Drinks & Wines</h2>
+          <p className="service-des">
+            The wines at the restaurant are all precious and of the best quality
+            for you enjoy
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default OurServices;
