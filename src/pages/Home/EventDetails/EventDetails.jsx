da38166604ea1dfb6d286b1b57df5109e89eb68e
Shahriar <shahriarh655@gmail.com>
2024-02-29 22:51:51 +0600

    Added events section

--- src/pages/Home/EventDetails/EventDetails.jsx
+++ src/pages/Home/EventDetails/EventDetails.jsx
@@ -4,25 +4,25 @@ import EventImages from "./EventImages";
 
 const EventDetails = () => {
   return (
-    <Container>
-      <div className="text-center space-y-5">
-        <h3 className="text-[#C3A981] italic text-lg">Private Event</h3>
-        <h1 className="text-[#DFE0E0] text-2xl sm:text-3xl md:text-4xl lg:text-5xl uppercase tracking-wide">
-          Perfect Place For Events
-        </h1>
-        <p className="text-[#DFE0E0] max-w-[60ch] mx-auto">
-          Whether you're hosting a corporate event, cocktail party, luncheon,
-          dinner, meeting, shower, wedding reception, bat/bar mitzvah or
-          rehearsal dinner, we know we can offer you and your guests a truly
-          memorable event.
-        </p>
-        <button className="px-4 py-2  md:px-6 md:py-3 lg:px-6 lg:py-3 text-[#DFE0E0] bg-[#C3A981] hover:bg-[#DFE0E0] hover:text-[#C3A981] transition-all duration-500 ">
-          Get Started For Your Event
-        </button>
-      </div>
-      {/* event details with images */}
+    <>
+      <Container>
+        <div className="text-center space-y-5">
+          <h3 className="event-details-sub-head">Private Event</h3>
+          <h1 className="event-details-main-head">Perfect Place For Events</h1>
+          <p className="event-details-des">
+            Whether you're hosting a corporate event, cocktail party, luncheon,
+            dinner, meeting, shower, wedding reception, bat/bar mitzvah or
+            rehearsal dinner, we know we can offer you and your guests a truly
+            memorable event.
+          </p>
+          <button className="event-details-btn">
+            Get Started For Your Event
+          </button>
+        </div>
+        {/* event details with images */}
+      </Container>
       <EventImages />
-    </Container>
+    </>
   );
 };
 
--- src/pages/Home/EventDetails/EventImages.jsx
+++ src/pages/Home/EventDetails/EventImages.jsx
@@ -5,7 +5,98 @@ import privateRoomImg from "../../../assets/Event-Img/private-room.jpg";
 import birthdayImg from "../../../assets/Event-Img/birthday.jpg";
 
 const EventImages = () => {
-  return <div>TODO</div>;
+  return (
+    <div className="event-images-container">
+      {/* item - 1 */}
+      <div className="item-1 group relative overflow-hidden mx-auto">
+        <div className="img ">
+          <picture>
+            <img src={meetingImg} alt="" />
+          </picture>
+          {/* over lay div */}
+          <div className="overlay-div">
+            <div className="event-img-text-container">
+              <div className="event-icon">
+                <h1>ICON</h1>
+              </div>
+              <h1 className="event-head">Meeting</h1>
+              <p className="text-[#DFE0E0]">
+                We are extremely happy to provide refined cater- ing services
+                for your special events! By choosing us, you get a guarantee
+                that everything.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* item - 2 */}
+      <div className="item-2 group relative overflow-hidden mx-auto">
+        <div className="img ">
+          <picture>
+            <img src={weddingImg} alt="" />
+          </picture>
+          {/* over lay div */}
+          <div className="overlay-div">
+            <div className="event-img-text-container">
+              <div className="event-icon">
+                <h1>ICON</h1>
+              </div>
+              <h1 className="event-head">Meeting</h1>
+              <p className="text-[#DFE0E0]">
+                We are extremely happy to provide refined cater- ing services
+                for your special events! By choosing us, you get a guarantee
+                that everything.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* item - 3 */}
+      <div className="item-3 group relative overflow-hidden mx-auto">
+        <div className="img">
+          <picture>
+            <img src={privateRoomImg} alt="" className="" />
+          </picture>
+          {/* over lay div */}
+          <div className="overlay-div">
+            <div className="event-img-text-container">
+              <div className="event-icon">
+                <h1>ICON</h1>
+              </div>
+              <h1 className="event-head">Meeting</h1>
+              <p className="text-[#DFE0E0]">
+                We are extremely happy to provide refined cater- ing services
+                for your special events! By choosing us, you get a guarantee
+                that everything.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* item - 4 */}
+      <div className="item-4 group relative overflow-hidden mx-auto">
+        <div className="img ">
+          <picture>
+            <img src={birthdayImg} alt="" />
+          </picture>
+          {/* over lay div */}
+          <div className="overlay-div">
+            <div className="event-img-text-container">
+              <div className="event-icon">
+                <h1>ICON</h1>
+              </div>
+              <h1 className="event-head">Meeting</h1>
+              <p className="text-[#DFE0E0]">
+                We are extremely happy to provide refined cater- ing services
+                for your special events! By choosing us, you get a guarantee
+                that everything.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EventImages;
